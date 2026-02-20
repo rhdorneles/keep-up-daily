6a818502e3f3d3ddfb530ec5c5c294f98ceabb62
Rafael Dorneles de Oliveira <82235550+rhdorneles@users.noreply.github.com>
2026-02-20 14:17:37 -0600

    Update __init__.py

--- scraper/sources/__init__.py
+++ scraper/sources/__init__.py
@@ -1,29 +1,12 @@
 """Source scrapers for keep-up-daily."""
 
-from .devto import DevtoScraper
-from .hackernews import HackerNewsScraper
-from .github_trending import GitHubTrendingScraper
-from .reddit import RedditScraper
-from .lobsters import LobstersScraper
-from .hashnode import HashnodeScraper
 from .sao_paulo_rss import SaoPauloRSS
 
 ALL_SCRAPERS = [
-    DevtoScraper,
-    HackerNewsScraper,
-    GitHubTrendingScraper,
-    RedditScraper,
-    LobstersScraper,
-    HashnodeScraper,
     SaoPauloRSS,
 ]
 
 __all__ = [
-    "DevtoScraper",
-    "HackerNewsScraper",
-    "GitHubTrendingScraper",
-    "RedditScraper",
-    "LobstersScraper",
-    "HashnodeScraper",
+    "SaoPauloRSS",
     "ALL_SCRAPERS",
 ]
